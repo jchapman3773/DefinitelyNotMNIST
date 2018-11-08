79140e6f47d151a79b9509af3e7008ba03c0798a
tdurnford <tjdford@gmail.com>
2018-11-08 14:43:44 -0700

    init

---
+++ src/clean_data.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import os
+import numpy as np
+from PIL import Image
+
+for i, letter in enumerate(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']):
+        directory = f'../large/{letter}/'
+        files = os.listdir(directory)
+        label = np.array([0]*10)
+        label[i] = 1
+        for file in files:
+            try:
+                im = Image.open(directory+file)
+            except:
+                print("Delete a corrupted file: " + file)
+                os.system(f'rm {directory+file}')
+                continue
